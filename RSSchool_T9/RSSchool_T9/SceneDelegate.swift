ad87be3b9959bf49ec958b68dcef3bc76640f7a1
JohnO <>
2021-07-29 23:31:50 +0300

    init: add app icons and init scene delegate

--- RSSchool_T9/RSSchool_T9/SceneDelegate.swift
+++ RSSchool_T9/RSSchool_T9/SceneDelegate.swift
@@ -14,9 +14,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = scene as? UIWindowScene else {
+        guard let windowScene = scene as? UIWindowScene else {
             fatalError("LOL, be careful, drink some water")
         }
+		let window = UIWindow(windowScene: windowScene)
+		
+		window.rootViewController = rootViewController
+		
+		self.window = window
+		window.makeKeyAndVisible()
     }
+	
+	var rootViewController: UIViewController {
+		let vc = UIViewController()
+		vc.view.layer.backgroundColor = UIColor.systemYellow.cgColor
+		return vc
+	}
 }
 
